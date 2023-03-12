230e6533a8ef4b2a2fed22a12c1210e5462ccf9c
geedtd <grcazares@gmail.com>
2023-03-12 16:32:57 -0700

    accidentally undefined dracoLoader last commit, re-defined it and changed vite server setttings to allow viewing on local network from other devices

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,8 @@ import { OrbitControls } from "@react-three/drei";
 import {Perf} from 'r3f-perf'
 import {useLoader} from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-// import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+
 
 export default function Experience() {
         // const dracoLoader = new DRACOLoader()
@@ -14,6 +15,7 @@ export default function Experience() {
             GLTFLoader,
             "./PortfolioBlock.glb",
             loader => {
+                const dracoLoader = new DRACOLoader
                 dracoLoader.setDecoderPath(
                     "https://www.gstatic.com/draco/v1/decoders/"
                 );
